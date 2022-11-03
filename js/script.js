c850cc5e6b39893a389aee9b081c59a66a931305
DiceglieA <angelodiceglie10@gmail.com>
2022-11-03 16:23:23 +0100

    repo + random num

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+let eleGrid = document.querySelector('.grid')
+let button = document.getElementById('play')
+let difficulty = document.getElementById('difficulty')
+button.addEventListener('click', play) 
+
+function bombe() {
+    let arrBombs = []
+    for (let i = 0; i < 16; i++) {
+        let bomb = Math.floor(Math.random() * difficulty.value * difficulty.value)
+        arrBombs.push(bomb)
+    }
+}
+
+function play() {
+    eleGrid.innerHTML = ''
+    for ( let i = 1; i <= difficulty.value * difficulty.value; i++) {
+    const eleCell = document.createElement('div')
+    eleCell.classList.add('cell')
+    eleCell.style.width = "calc(100% / "+difficulty.value+")"
+    eleCell.innerHTML = `${i}`
+    eleGrid.append(eleCell)
+
+    
+    eleCell.addEventListener('click', function () {
+        this.classList.toggle('active')  
+    }) 
+}
+}
+
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
